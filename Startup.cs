746b7e9d02eeba5c07429c1055c42df642b88e6f
Rogenis Silva <rps@icomp.ufam.edu.br>
2023-08-14 09:58:39 -0400

    fix config

--- Startup.cs
+++ Startup.cs
@@ -3,8 +3,10 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microservice.Repositories;
+using Microsoft.Azure.Cosmos;
 using Microservice.Services;
+using Microservice.Repositories;
+using System;
 
 namespace Microservice
 {
@@ -19,10 +21,21 @@ namespace Microservice
 
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddScoped<ServiceBusQueue>();
             services.AddScoped<CosmosDbService>();
+            // Configuração do Service Bus
+            services.AddScoped<ServiceBusQueue>();
+
+            // Configuração do Cosmos DB
+            services.AddSingleton<CosmosClient>(InitializeCosmosClient);
             services.AddScoped(typeof(CosmosDbRepository<>));
+
             services.AddControllers();
+        }
+
+        private CosmosClient InitializeCosmosClient(IServiceProvider serviceProvider)
+        {
+            string connectionString = Configuration.GetConnectionString("CosmosDBConnection");
+            return new CosmosClient(connectionString);
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
