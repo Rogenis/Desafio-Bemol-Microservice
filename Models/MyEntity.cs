e6b76f06b34af86a5864ac2fa64fd4b8ac29694d
Rogenis Silva <rps@icomp.ufam.edu.br>
2023-08-14 09:22:21 -0400

    add models

---
+++ Models/MyEntity.cs
@@ -0,0 +1,9 @@
+namespace Microservice.Models
+{
+    public class MyEntity
+    {
+        public string Id { get; set; }
+        public string Key { get; set; }
+        public string AnotherKey { get; set; }
+    }
+}
